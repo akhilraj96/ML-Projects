ea7c1966a1266ad8938e452a611d1993b08cf4a1
Akhil Raj <akhil_raj@outlook.com>
2023-05-17 02:28:44 +0100

    edits

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -93,7 +93,7 @@ class DataTransformation:
 
             logging.info("Applying preprocessing object on training and testing dataframe")
 
-            input_feature_train_arr = prepocessing_obj.fit_transform(input_feature_train_df,target_feature_train_df)
+            input_feature_train_arr = prepocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = prepocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
