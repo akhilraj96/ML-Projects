e4889ae6f89b6e4952a7a0e32110c6d897771fe2
Akhil Raj <akhil_raj@outlook.com>
2023-07-06 01:03:13 +0100

    Predicition Pipeline

--- src/utils.py
+++ src/utils.py
@@ -22,24 +22,34 @@ def save_object(file_path, obj):
 
 def evaluate_models(x_train,y_train,x_test,y_test,models,params):
 
-    report = {}
-    
-    for i in range(len(list(models))):
-        model = list(models.values())[i]
-        param=params[list(models.keys())[i]]
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
-        gs = GridSearchCV(model,param,cv=3)
-        gs.fit(x_train,y_train)
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
 
-        model.set_params(**gs.best_params_)
-        model.fit(x_train,y_train) # Train model
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train) # Train model
 
-        y_train_pred = model.predict(x_train)
-        y_test_pred = model.predict(x_test)
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
 
-        train_model_score = r2_score(y_train,y_train_pred)
-        test_model_score = r2_score(y_test,y_test_pred)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
 
-        report[list(models.keys())[i]]=test_model_score
-    
-    return report
+            report[list(models.keys())[i]]=test_model_score
+        
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
